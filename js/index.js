7ab148ee5cb5f0b3bbc25288fa92fa0baa6ca6b5
cynthiagaitho <cynthiagaitho6@gmail.com>
2024-07-24 16:17:13 +0300

    moved files

--- js/index.js
+++ js/index.js
@@ -1,12 +1,21 @@
 document.addEventListener("DOMContentLoaded", function (event) {
+
     var thumbnailElement = document.getElementById("smart_thumbnail");
     thumbnailElement.addEventListener("click", function () {
       // write here
-  
+  console.log('clicked')
       if (thumbnailElement.className === "small") {
         thumbnailElement.className = "";
       } else {
         thumbnailElement.className = "small";
       }
     });
+
+    var clickMeBtn = document.getElementById('click-me-btn')
+    clickMeBtn.addEventListener('click',function(){
+      console.log('clicked')
+      const result = prompt('Hello visitor. What is your name?')
+
+      alert(`Hello ${result} welcome to my webpage. Feel free to explore.`)
+    })
   });
